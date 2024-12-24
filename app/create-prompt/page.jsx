36f9d09b14e76beeb82ce9d9fce6c90cc5ef4c57
sweatha-svG <152466567+sweatha-svG@users.noreply.github.com>
2024-12-24 00:39:05 -0800

    project1-nextjs

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -3,7 +3,7 @@ import {useState} from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation' // Updated import
 
-import Form from '@components/Form'
+import Form from '@component/Form'
 
 
 const CreatePrompt = () => {
